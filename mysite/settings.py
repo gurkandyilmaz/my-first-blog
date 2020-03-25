97d592452efff69005fdf056761fa5d861167195
gurkan yilmaz <gurkandy@gmail.com>
2020-03-25 21:58:02 +0300

    new styles added to the css file

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,8 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print('BASE_DIR: ', BASE_DIR)
-TEMPLATES_DIR = BASE_DIR + '/templates'
+
+TEMPLATES_DIR = BASE_DIR + '/templates/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
