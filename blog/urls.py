db36be8f4f1115643c13e6b771fa3a551cf61dec
gurkan yilmaz <gurkandy@gmail.com>
2020-03-25 23:31:58 +0300

    Added view and template for showing single post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 
 urlpatterns = [
    
     path('', views.post_list, name='post_list'),
+    path('show-post/<int:primary_key>/', views.show_post, name='show-post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Post
@@ -10,3 +10,9 @@ def post_list(request):
 	content_dict = {'all_posts':posts}
 
 	return render(request, 'blog/post_list.html', context=content_dict)
+
+def show_post(request, primary_key):
+	post = get_object_or_404(Post, pk=primary_key)
+
+
+	return render(request, 'blog/show_post.html', {'single_post':post})
